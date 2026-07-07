03dcfa3339f6cde9f2f97f050dc525d958a6b9c4
communication <communication@campfrance.fr>
2026-07-07 23:30:59 +0200

    Détecter le mot dicté "étoile" et utiliser les icônes officielles Prioris
    La saisie vocale reconnaît "étoile" n'importe où dans la phrase (peu
    importe l'ordre des blocs), coche l'étoile de la tâche et retire le mot
    de la remarque avant enregistrement. Remplace aussi les icônes générées
    par les icon-192.png / icon-512.png fournis, et met à jour le manifest
    et le cache du service worker en conséquence.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'prioris-v1';
-const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
+const CACHE = 'prioris-v2';
+const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
